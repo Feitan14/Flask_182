24513306b4f5aac010870dcfb8d25212e5d4c8f8
Yo <andy28morales@gmail.com>
2023-06-15 18:45:25 -0600

    rutas y variables

--- practica5/app.py
+++ practica5/app.py
@@ -1,10 +1,28 @@
 from flask import Flask
 
+#inicializacion del servidor Flask
 app= Flask(__name__)
+app.config['MySQL_HOST'] = "localhost"
+app.config['MySQL_USER'] = "root"
+app.config['MySQL_PASSWORD'] = ""
+app.config['MySQL_BD'] = "bdflask"
 
+#declaramos una ruta
+#ruta se compone de nombre y funcion 
+#ruta index o ruta principal: (http://localhost:5000)
 @app.route('/')
 def index():
     return "Hola mundo"
 
+@app.route('/guardar')
+def guardar():
+    return "se guardo el album en la BD"
+
+@app.route('/eliminar')
+def eliminar():
+    return "se elimino el album en la BD"
+
+
+#ejecutan el servidor
 if __name__=='__main__':
-    app.run(port= 5000)
+    app.run(port= 5000, debug=True)
